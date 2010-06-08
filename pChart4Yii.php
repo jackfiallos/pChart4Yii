3a8346f6fdb64f2d71410d0d148e9ed969fac5ff
Jackfiallos <erling.fiallos@qbit.com.mx>
2010-06-08 16:28:22 -0500

    libraries added

---
+++ pChart4Yii.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'pChart/pChart.class.php';
+
+class pChart4Yii extends pChart
+{
+}
---
+++ pData4Yii.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'pChart/pData.class.php';
+
+class pData4Yii extends pData
+{
+}
